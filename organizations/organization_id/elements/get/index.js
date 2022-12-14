d782e1ed60e7f9868c8a2065bf948c04893ad0a4
Kin Lane <kinlane@gmail.com>
2022-12-13 20:31:44 -0800

    fix id

--- organizations/organization_id/elements/get/index.js
+++ organizations/organization_id/elements/get/index.js
@@ -11,7 +11,7 @@ exports.handler = vandium.generic()
     database : process.env.database
     });
 
-    var sql = 'SELECT * FROM elements e INNER JOIN organizations_elements oe ON e.id = oe.element_id WHERE oe.organization_id = ' + event.organization_id + ' ORDER BY t.Name';
+    var sql = 'SELECT DISTINCT e.id,e.name FROM elements e INNER JOIN organizations_elements oe ON e.id = oe.element_id WHERE oe.organization_id = ' + event.organization_id + ' ORDER BY e.Name';
     connection.query(sql, function (error, results, fields) {
     callback( null, results );
   });
